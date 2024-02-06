235bb9de3b0b41261bbf071a4e3d2daddf394c17
surbhixyyz <surbhi112122@gmail.com>
2024-02-07 00:49:36 +0530

    Get involved page along with styling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Login from './components/Login.jsx';
 import SignUp from './components/SignUp.jsx';
 import Home from './components/Home.jsx';
 import GetInvolved from './components/getInvolved.jsx';
+import AskExpert from './components/AskExpert.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -18,6 +19,7 @@ const AppRouter = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
         <Route path="/get-involved" element={<GetInvolved />} />
+        <Route path="/ask-expert" element={<AskExpert />} />
       </Routes>
     </Router>
   );
