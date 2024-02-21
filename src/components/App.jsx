713a13efd43bb74374711dd702478368cd04ee3d
Priyanshi <priyanshi.7603@gmail.com>
2024-02-21 18:03:01 +0530

    ServiceLocator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,15 @@ import EduHub from "./EduHub";
 import Navbar from "./Navbar";
 import { BrowserRouter } from "react-router-dom";
 import '../index.css'
+import LatLong from "./LatLong";
+
+
 
 function App(){
-
     return (<div>
         <BrowserRouter>
-       <EduHub></EduHub>
+        {/* <EduHub></EduHub> */}
+     <LatLong></LatLong>
        </BrowserRouter>
     </div>
     );
